9d3c9b2b08b4d7a00089874950a6a1c4dd50b14b
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2021-09-09 16:03:26 +0200

    add back button to navigate from reviews to homescreen

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import { globalStyles } from '../styles/global';
 
-export default function ReviewDetails() {
+export default function ReviewDetails({navigation}) {
+
+  const pressHandler = () => {
+     navigation.navigate('Home')
+  }
    return (
      <View style={globalStyles.container}>
        <Text>Review detail Screen</Text>
+       <Button title={'Back'} onPress={pressHandler}/>
      </View>
    )
 }
